3d637d3637695573a49df8a37fc2efc23628e165
suryak7 <suryakesavan50@gmail.com>
2016-07-06 00:27:29 +0530

    Add files via upload

---
+++ helwr.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char word[1000];
+int con,i,len;
+scanf("%s",word);
+scanf("%d",&con);
+len=strlen(word);
+for(i=0;i<len;i++)
+{ if(i==0)
+{
+printf("%c",word[0]);
+}
+else
+if(i%con!=0)
+{
+printf("%c",word[i]);
+}
+}
+getch();
+}
